822d44d6037671dc0f42054065e005eb379f03ae
bentleybiscuit <charlottenightingale@ymail.com>
2019-10-08 14:56:04 +0100

    Update functions/gameStarted.js
    Added import for shuffle

--- functions/gameStarted.js
+++ functions/gameStarted.js
@@ -1,4 +1,5 @@
 import {game} from '../main.js'
+import 'shuffle.js'
 //create eventListener for New Game html element with params of game object and shuffled array
 document.querySelector('.newGame').addEventListener('click', function (e) {
     e.stopImmediatePropagation()
