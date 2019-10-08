b42229544c3810a6818f2a969e029cacbee49e93
Josh Selby <jdselby24@gmail.com>
2019-10-08 15:02:36 +0100

    Moved stopImmediatePropogation to top of callback

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ var cards = document.querySelectorAll('.card')
 //forEach loop that calls displayImage function when a card is clicked
 cards.forEach(function(card) {
     card.addEventListener('click', function(e){
+        e.stopImmediatePropagation()
         let cardId = e.id
         displayImage(game.cardArray, cardId)
-        e.stopImmediatePropagation()
     })
 })
