6e02449fe666a784065f52034fd9eb94eb7aa835
sudokufan <alexrichardgeorge@protonmail.com>
2019-10-08 13:48:34 +0100

    created forEach loop to run displayImage function when a card is clicked

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var cards = document.querySelectorAll('.card')
 
+//forEach loop that calls displayImage function when a card is clicked
 cards.forEach(function(card) {
     card.addEventListener('click', function(e){
-        let cardId = e.Id
+        let cardId = e.id
+        displayImage(game.cardArray, cardId)
     })
 })
