d85789207a2f0f1f6b55c0d7f2b3c956d2e83866
jdselby24 <jdselby24@gmail.com>
2019-10-08 15:59:59 +0100

    removed extraneous console.log

--- functions/displayImage.js
+++ functions/displayImage.js
@@ -5,7 +5,6 @@
  * @return {number} int takes id from html file and used as a key to get value from array and attach to string in new background image
  */
 function displayImage (array, id) {
-	console.log(id)
 	let card = document.getElementById(id)
     card.style.backgroundImage = "url('images/gecko" + array[id] + ".jpg')"
 }
