ba95f908f2fec9b83b281420dc2d28a92f9be35c
bentleybiscuit <charlottenightingale@ymail.com>
2019-10-08 15:07:26 +0100

    removed if

--- functions/gameStarted.js
+++ functions/gameStarted.js
@@ -1,12 +1,9 @@
 import {game} from '../main.js'
 import 'shuffle.js'
 //create eventListener for New Game html element with params of game object and shuffled array
-document.querySelector('.newGame').addEventListener('click', function (e) {
+document.querySelector('.newGame').addEventListener('click', function(e) {
     e.stopImmediatePropagation()
-    if (game.gameStarted = true) {
-        document.querySelector('.cards').styles.backgroundImage="url('images/cardBack.jpg')"
-    } else {
-        game.gameStarted = true
-        game.cardArray = shuffle(game.cardArray)
-    }
-})
+    document.querySelector('.cards').styles.backgroundImage="url('images/cardBack.jpg')"
+    game.cardArray = shuffle(game.cardArray)
+)
+}
