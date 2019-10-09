0d2878c8b9572e4e8e8a1df106a717f6a094c3f9
sudokufan <alexrichardgeorge@protonmail.com>
2019-10-09 14:53:46 +0100

    adjusted CSS codes for instructions hidden/visibility

--- main.js
+++ main.js
@@ -20,10 +20,10 @@ document.querySelector('.newGame').addEventListener('click', function(e) {
 
 document.querySelector('.instructionsButton').addEventListener('click', function(e){
     let instructions = document.querySelector('.instructions')
-    if (instructions.style.display == "none") {
-        instructions.style.display = "block"
+    if (instructions.classList = "instructions hidden") {
+        instructions.classList = "instructions visible"
     } else {
-        instructions.style.display = "none"
+        instructions.classList = "instructions hidden"
     }
 })
 
