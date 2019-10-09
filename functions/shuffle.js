834326f08c93c05adc0690b4a4530301f62c7189
Josh Selby <jdselby24@gmail.com>
2019-10-09 09:19:03 +0100

    Removed semicolon

--- functions/shuffle.js
+++ functions/shuffle.js
@@ -13,7 +13,7 @@
 function shuffle(arr) {
     let i, j, temp
     for (i = arr.length - 1; i > 0; i--) {
-        j = Math.floor(Math.random() * (i + 1));
+        j = Math.floor(Math.random() * (i + 1))
         temp = arr[i]
         arr[i] = arr[j]
         arr[j] = temp;
