81f63c4b082f97728af9e966e1779d246743f9f7
Josh Selby <jdselby24@gmail.com>
2019-10-09 09:23:37 +0100

    Rmeoved duplicated cards var

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 let game = {
-    cardArray: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
+    cardArray: [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8],
+    cards: document.querySelectorAll('.card')
 }
 
 game.cardArray = shuffle(game.cardArray)
 
-// Get cards from DOM and store them as a NodeList
-var cards = document.querySelectorAll('.card')
-
 document.querySelector('.newGame').addEventListener('click', function(e) {
     e.stopPropagation
-    let cards = document.querySelectorAll('.card')
-    cards.forEach(function(card) {
+    
+    game.cards.forEach(function(card) {
     	card.style.backgroundImage="url('images/cardBack.jpg')"
     })
     game.cardArray = shuffle(game.cardArray)
@@ -26,10 +24,10 @@ document.querySelector('.instructionsButton').addEventListener('click', function
 })
 
 //forEach loop that calls displayImage function when a card is clicked
-cards.forEach(function(card) {
+game.cards.forEach(function(card) {
     card.addEventListener('click', function(event){
         event.stopImmediatePropagation()
-        let cardId = event.target.id
+        let cardId = this.id
         displayImage(game.cardArray, cardId)
     })
 })
